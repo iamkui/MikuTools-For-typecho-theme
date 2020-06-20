421be69180ef9ed3edba9ec8b0142844e1fb23ac
Kui.Li <41515711+iamkui@users.noreply.github.com>
2020-06-20 19:55:55 +0800

    修改首页

--- usr/themes/MiKuTools/index.php
+++ usr/themes/MiKuTools/index.php
@@ -14,26 +14,25 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 
             <?php Typecho_Widget::widget('Widget_Stat')->to($stat); ?>
-          <?php $this->widget('Widget_Metas_Category_List')->to($categories); ?>
-              <?php while ($categories->next()): ?>
+  			  <?php $this->widget('Widget_Metas_Category_List')->to($categories); ?>
+      		    <?php while ($categories->next()): ?>
                   <?php if(count($categories->children) === 0): ?>
                   <?php $this->widget('Widget_Archive@category-' . $categories->mid, 'pageSize=10000&type=category', 'mid=' . $categories->mid)->to($posts); ?>
 
-<fieldset class="nya-container">
-          <legend class="nya-title">
-            <i class="fa <?php $categories->slug(); ?>"></i>
-            <span><?php $categories->name(); ?></span>
-          </legend><br>
+					<fieldset class="nya-container">
+						<legend class="nya-title">
+							<i class="fa <?php $categories->slug(); ?>"></i>
+							<span><?php $categories->name(); ?></span>
+					</legend><br>
 
-                  <?php while ($posts->next()): ?><a href="<?php $posts->test_url(); ?>" target="_blank" rel="nofollow" class="nya-btn <?php $posts->test_word(); ?>" title="<?php $posts->title(); ?>">
+                		<?php while ($posts->next()): ?><a href="<?php $posts->permalink() ?>" rel="nofollow" class="nya-btn <?php $posts->test_word(); ?>" title="<?php $posts->title(); ?>">
                            <?php $posts->title(); ?>
-</a><?php endwhile; ?>
-</fieldset>
+						</a><?php endwhile; ?>
+					</fieldset>
 
 
-        <?php else: ?>
-      <?php endif; ?>
-  <?php endwhile; ?>
-
-       <?php $this->need('global.php'); ?>
+              <?php else: ?>
+            <?php endif; ?>
+        <?php endwhile; ?>
+ <?php $this->need('global.php'); ?>
 <?php $this->need('footer.php'); ?>
